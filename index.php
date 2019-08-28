9044c4196871711b6511a9b27a66c76a52f6145a
PhilippeGambette <philippe.gambette@laposte.net>
2019-08-28 17:46:22 +0200

    mise à jour visuels + page d'accueil du jeu

--- index.php
+++ index.php
@@ -12,17 +12,29 @@ include ('jeuConnexion.php');
     <title><?php echo $titreJeu; ?></title>
     <link rel="shortcut icon" href="./img/favicon.ico">
     <link rel="stylesheet" href="css/style.css">
+    <script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+    <script src="./js/matrimoineGo.js"></script>
 </head>
 
 <body class="body2">
     <!--<h1 class="titreBlanc"><?php echo $titreJeu; ?> </h1>-->
-    <div style="width:100%;text-align:center;"><img src="./img/logo.png" alt="Matrimoine GO - Paris" style="width:50%;text-align:center;max-width:250px;margin:20px;"></div>
+    <div style="width:100%;text-align:center;">
+       <img src="./img/logo.png" alt="Matrimoine GO - Paris" style="width:50%;text-align:center;max-width:250px;margin:20px;">
+    </div>
     <div class="containerBut">
-        <p><?php echo $bienvenue; ?></p>
+        <p style="margin-left:20%;margin-right:20%"><?php echo $bienvenue; ?></p>
         <a href="choix_jeux.php" class="but">Commencer !</a>
     
-        <div class="bas"><small><i>Un <a href="https://github.com/Avgilles/jeuxCiteDesDames/">jeu</a> créé par <a href="http://gillesavraam.com">Gilles Avraam</a>, sur un concept d'<a href="http://matrimoinedeparis.com">Edith Vallée</a></i></small></div>
+        <div class="bas"><small><i>Un <a href="https://github.com/Avgilles/jeuxCiteDesDames/">jeu</a> réalisé par <a href="http://gillesavraam.com">Gilles Avraam</a>, sur un concept d'<a href="http://matrimoinedeparis.com">Edith Vallée</a> d'après son livre <i><a href="http://matrimoinedeparis.com/">Le matrimoine de Paris</a></i></i></small>.<br/>
+            <button class="nbrErreur js-aPropos" style="background-color:#999999;">À propos...</button>
+        </div>
     </div>
 </body>
+<script type="text/javascript">
+// Détection du clic sur le bouton "vérifier"
+$(document).on("click",".js-aPropos",function() {
+    pop_up("<p><strong>À propos de Matrimoine Go</strong></p><?php echo $a_propos; ?><p>Le moteur de ce jeu est disponible sous licence GPL <a href=\"https://github.com/Avgilles/jeuxCiteDesDames\">sur GitHub</a>. Il a été réalisé par <a href=\"http://gillesavraam.com\">Gilles Avraam</a> sur un concept d'<a href=\"http://matrimoinedeparis.com\">Edith Vallée</a> d'après son livre <i><a href=\"http://matrimoinedeparis.com/\">Le matrimoine de Paris</a></i></i></small>. Le code de ce jeu a été programmé par Gilles pendant son stage de première année de <a href=\"http://www.dut-mmi-champs.fr/\">DUT MMI</a> à l'<a href=\"http://iut.u-pem.fr\">IUT de Marne-la-Vallée</a>, stage réalisé au <a href=\"http://ligm.u-pem.fr\">LIGM</a> dans le cadre du projet de recherche <a href=\"http://citedesdames.hypotheses.fr\">Cité des dames</a>, encadré par <a href=\"http://igm.univ-mlv.fr/~gambette\">Philippe Gambette</a>.</p><p>Le code programmé en PHP, MySQL et Javascript utilise notamment les bibliothèques <a href=\"http://jquery.com\">jQuery</a>, <a href=\"https://jqueryui.com/\">jQuery UI</a>, <a href=\"http://touchpunch.furf.com/</p>\">jQuery UI TouchPunch</a>, <a href=\"https://www.mapbox.com/\">mapbox</a>, des données d'<a href=\"https://www.openstreetmap.org\">OpenStreetMap</a> et d'<a href=\"https://osmbuildings.org/\">OSM Buildings</a> et des icônes de <a href=\"https://www.flaticon.com/\">flaticon.com</a>","","",false,true);
+})
 
+</script>
 </html>
