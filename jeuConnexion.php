f67472fe912faebf6a87853274821b664f14a49a
PhilippeGambette <philippe.gambette@laposte.net>
2019-08-28 17:57:40 +0200

    ajout de variable $a_propos dans jeuConnexion.php

--- jeuConnexion.php
+++ jeuConnexion.php
@@ -8,17 +8,15 @@ $link = new PDO('mysql:host=127.0.0.1;dbname=jcdd', 'root', '', array(PDO::MYSQL
 
 $clefApiMapbox= 'clef_mapbox';
 
+$bienvenue=" <p><strong>Bienvenue sur MatrimoineGo !</strong></p><p>Grâce à MatrimoineGo tu vas pouvoir connaître les grandes artistes du premier arrondissement de Paris. Le but est de retrouver quelle femme correspond à quel emplacement sur la carte.</p><p>Tu peux le faire depuis chez toi ou en allant sur place:</p>";
+
+$accueil_jeu="Relie chaque portait de chaque femme en le déplaçant avec ton doigt, mais attention à ton score ! Si tu te trompes celui-ci descendra, par contre si tu réussis il augmentera ! Tu peux t'aider des indices en appuyant sur le point d'interrogation en haut des photos de femme ou en appuyant sur les marqueurs sur la carte. Bonne chance !";
 
 
-$accueil_jeu="Relis chaque Portait de chaque femme en les déplaçant avec ton doigt, mais attention à ton score ! Si tu te trompes celui-ci descendras, par contre si tu réussis il augmentera ! Tu peux t'aider des indices en appuyant sur le point d'interrogation en haut des photos de femme ou en appuyant sur les marqueurs. Bonne chance !";
-
-
-$accueil_drag="Classe les artistes ensemble, grâce aux informations que tu as eu à la première étape tu peux maintenant classer ces femmes en 2 catégories la scène et le salon. Déplace les photos des femmes vers l'endroit qui te semble le plus approprié.";
+$accueil_drag="Classe les artistes ensemble, grâce aux informations que tu as eues à la première étape tu peux maintenant classer ces femmes en 2 catégories, la scène et le salon. Déplace les photos des femmes vers l'endroit qui te semble le plus approprié.";
 
 $choix_jeu="Choisis le parcours que tu veux effectuer.";
 
-    
-$bienvenue=" <p><strong>Bienvenue sur MatrimoineGo !</strong></p><p>Grâce à MoitrimoineGo tu vas pouvoir connaître les grandes artistes de ta ville.Le but est de retrouver quelle femmes correspond à quelle oeuvres.</p><p>Tu peux décider de le faire depuis chez toi ou en allant sur place:</p>";
-
+$a_propos = "<p>Concept du jeu par <a href=\\\"http://matrimoinedeparis.com\\\">Edith Vallée</a>.</p>";
 
 ?>
