4878f2e5c01307bfd1de2151a1130041b5d91763
PhilippeGambette <philippe.gambette@laposte.net>
2019-08-28 12:11:04 +0200

    ajout logo index

--- index.php
+++ index.php
@@ -1,49 +1,28 @@
 <!DOCTYPE html>
 <html lang="fr">
-
+<?php
+// Chargement des données de configuration du jeu
+include ('jeuConnexion.php');
+?>
 <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="Jeux cité des dames">
-    <title>
-    <?php
-    $titreJeu = "MatrimoineGo";
-    echo $titreJeu;
-    ?>
-    </title>
-<!--    <link rel="shortcut icon" href="">-->
+    <meta name="description" content="<?php echo $titreJeu; ?>">
+    <title><?php echo $titreJeu; ?></title>
+    <link rel="shortcut icon" href="./img/favicon.ico">
     <link rel="stylesheet" href="css/style.css">
-
 </head>
 
 <body class="body2">
-   <h1 class="titreBlanc"><?php echo $titreJeu; ?> </h1>
-   
+    <!--<h1 class="titreBlanc"><?php echo $titreJeu; ?> </h1>-->
+    <div style="width:100%;text-align:center;"><img src="./img/logo.png" alt="Matrimoine GO - Paris" style="width:50%;text-align:center;max-width:250px;margin:20px;"></div>
     <div class="containerBut">
-
-        <?php
+        <p><?php echo $bienvenue; ?></p>
+        <a href="choix_jeux.php" class="but">Commencer !</a>
     
-        //----------chargement du site soit local soit université---------------------------
-
-            include ('jeuConnexion.php');
-        //----------chargement du site soit local soit université---------------------------
-        echo $bienvenue
-            
-            
-        ?>
-      
-       
-      
-    
-           
-
-       <a href="choix_jeux.php" class="but">Commencer !</a>
-        
+        <div class="bas"><small><i>Un <a href="https://github.com/Avgilles/jeuxCiteDesDames/">jeu</a> créé par <a href="http://gillesavraam.com">Gilles Avraam</a>, sur un concept d'<a href="http://matrimoinedeparis.com">Edith Vallée</a></i></small></div>
     </div>
-    
-    <div class="bas">Créer par GILLES AVRAAM</div>   
-       
 </body>
 
 </html>
